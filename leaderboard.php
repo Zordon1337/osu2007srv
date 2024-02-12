5ce0762c0a1cafce94b1e416fbd73f4e17a9760b
Zordon <65111609+Zordon1337@users.noreply.github.com>
2024-02-12 15:34:26 +0100

    some general cleanup

--- leaderboard.php
+++ leaderboard.php
@@ -7,72 +7,9 @@ ini_set('display_errors', 0);
 <head>
     <meta charset='UTF-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-    <title>osu2007srv</title>
+    <title>Leaderboard</title>
     <style>
-        body {
-            background-color: #1a1a1a;
-            color: #ffffff;
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-        }
-
-        #container {
-            border: 2px solid #000000;
-            padding: 20px;
-            text-align: center;
-        }
-
-        #playerNameLabel {
-            display: block;
-            margin-bottom: 10px;
-        }
-
-        #playerNameInput {
-            width: 200px;
-            padding: 8px;
-            box-sizing: border-box;
-            margin-bottom: 20px;
-        }
-
-        #searchButton {
-            background-color: #4CAF50;
-            color: #ffffff;
-            padding: 10px 20px;
-            font-size: 16px;
-            cursor: pointer;
-        }
-
-        #searchButton:hover {
-            background-color: #45a049;
-        }
-        #playerInfo {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            margin-bottom: 20px;
-        }
-        table {
-            width: 100%;
-            border-collapse: collapse;
-            margin-top: 20px;
-            
-        }
-
-        th, td {
-            border: 1px solid #ddd;
-            padding: 8px;
-            text-align: left;
-        }
-
-        th {
-          background-color: #f2f2f2;
-          color:black;
-        }
+        @import url("main.css");
     </style>
 </head>
 <body>
--- profile.php
+++ profile.php
@@ -14,56 +14,7 @@ echo "<!DOCTYPE html>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <title>osu2007srv</title>
     <style>
-        body {
-            background-color: #1a1a1a;
-            color: #ffffff;
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-        }
-
-        #container {
-            border: 2px solid #000000;
-            padding: 20px;
-            text-align: center;
-        }
-
-        #playerNameLabel {
-            display: block;
-            margin-bottom: 10px;
-        }
-
-        #playerNameInput {
-            width: 200px;
-            padding: 8px;
-            box-sizing: border-box;
-            margin-bottom: 20px;
-        }
-
-        #searchButton {
-            background-color: #4CAF50;
-            color: #ffffff;
-            padding: 10px 20px;
-            font-size: 16px;
-            cursor: pointer;
-        }
-
-        #searchButton:hover {
-            background-color: #45a049;
-        }
-        #playerInfo {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            margin-bottom: 20px;
-        }
-        img {
-            border: 2px solid white;
-        }
+        @import url('main.css');
     </style>
 </head>
 <body>
@@ -75,8 +26,14 @@ if(CheckIfUserExists($conn,$username))
     $score = number_format(GetTotalScoreByUser($conn,$username));
     $rank = number_format(GetRank($conn,$username));
     $img = GetPfp($conn,$username);
+    $add = "";
+    if(CheckIfAdmin($conn,$username))
+    {
+        $add = "<p id='adminBadge'>ADMIN</p>";
+    }
 echo " 
 <img src='/forum/download.php?avatar=$img' height='186' width='186'/>
+$add
 <pre>
 Accuracy: $accuracy%<br/>
 Total Score: $score<br/>
