eff4a49c2ca374bbf72af298a0efb6c7ae572526
Zordon <65111609+Zordon1337@users.noreply.github.com>
2024-02-13 22:28:21 +0100

    added ban api

---
+++ api/banstatus.php
@@ -0,0 +1,11 @@
+<?php
+include("utils/db.php");
+$username = $_GET['u'];
+if(CheckIfBanned($username))
+{
+    $days = GetBanExpiration($conn,$username);
+    echo "1|$days";
+} else {
+    echo "0";
+}
+?>
